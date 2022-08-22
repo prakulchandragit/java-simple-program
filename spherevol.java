d527598886270bf5e8b6d5f0e7fbb7df56c32a37
prakulchandragit <thedictator822gmail.com>
2022-08-22 21:04:44 +0530

    new program

---
+++ spherevol.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class sphere {
+    double radius, volume;
+
+    void input() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Radius");
+        radius = sc.nextDouble();
+    }
+
+    void findvol() {
+        volume = 4 / 3.0 * 3.14 * Math.pow(radius, 3); // class metho
+    }
+
+    void display() {
+        findvol(); // nested method calling
+        System.out.println(radius + " " + volume);
+    }
+
+    public static void main(String args[]) {
+        sphere ob = new sphere(); // method declaration
+        ob.input();
+        ob.display();
+    }
+}
