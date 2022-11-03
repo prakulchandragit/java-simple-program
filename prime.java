3023ea161bd3b8fa900cfe48eb0f6f6b52093c6a
prakulchandragit <thedictator822gmail.com>
2022-11-03 21:12:41 +0530

    new program

---
+++ prime.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class prime {
+
+    public static void main(String args[]) {
+        int c = 0;
+        int i;
+        Scanner sn = new Scanner(System.in);
+        System.out.println("enter num");
+        int n = sn.nextInt();
+
+        for (i = 2; i <= n / 2; i++)
+            if (n % i == 0)
+                c = 1;
+
+        if (c == 0)
+            System.out.println("isprime");
+        else
+            System.out.println("not prime");
+
+    }
+
+}
