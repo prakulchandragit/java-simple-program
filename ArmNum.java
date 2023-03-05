15512760682b4364689085a703c569eef9b6f750
prakulchandragit <thedictator822gmail.com>
2023-03-05 21:05:51 +0530

    new program

---
+++ ArmNum.java
@@ -0,0 +1,37 @@
+class ArmNum {
+    int n;
+    int len;
+    int sum = 0;
+
+    ArmNum(int nn) {
+        n = nn;
+    }
+
+    int sum_pow(int i) {
+        int numOfDigits = String.valueOf(n).length();
+
+        int temp = n;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, numOfDigits);
+            temp /= 10;
+        }
+        return sum;
+
+    }
+
+    void isArmstrong() {
+        if (sum == n) {
+            System.out.println("it ij armstrong");
+        } else {
+            System.out.println("tmkc");
+        }
+    }
+
+    public static void main(String args[]) {
+        ArmNum ob = new ArmNum(69);
+        ob.sum_pow(0);
+        ob.isArmstrong();
+
+    }
+}
--- Change.java
+++ Change.java
@@ -1,14 +1,41 @@
+import java.util.StringTokenizer;
+
 public class Change {
     String str;
     String nstr;
-    Change(String X)
-    {
-        X=str;
-    }
-    String reverse (String w)
-    {
-        
-        return w;
 
+    Change(String X) {
+        X = str;
+    }
+
+    StringTokenizer st = new StringTokenizer(str);
+
+    String reverse(String w) {
+        String wt = "";
+        String wter = "";
+        while (st.hasMoreTokens()) {
+            wt = st.nextToken();
+        }
+        if (wt.indexOf("aeiouAEIOU") != -1) {
+            return wt;
+        }
+        wter = wt;
+        return wter;
+    }
+
+    void display() {
+        String nigga = "";
+        while (st.hasMoreTokens()) {
+            nigga = st.nextToken();
+        }
+        nstr = nigga + str;
+        System.out.println(str);
+        System.out.println(nstr);
+    }
+
+    public static void main(String args[]) {
+        Change ob = new Change("THE OWL AND THE EAGLE");
+        ob.reverse(null);
+        ob.display();
     }
 }
